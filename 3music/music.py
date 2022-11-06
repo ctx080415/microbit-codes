238db5fd2414af31046354f67ae4b3ec9f2ead22
ctx080415 <65986138+ctx080415@users.noreply.github.com>
2022-11-06 15:57:54 +0900

    음악 재생기
    3. 음악 재생기

--- 3music/music.py
+++ 3music/music.py
@@ -6,7 +6,7 @@ song_hello = ["C4:4", "D", "E", "C", "C", "D", "E", "C", "E", "F", "G:8", "E:4",
 carol =  [ 'e:2','e:2','e:4', 'e:2','e:2','e:4', 'e:2','g:2','c','d', 'e:8'] #크리스마스 캐롤
 
 while True:
-    if button_a.is_pressed():
-        music.play(carol)
-    elif button_b.is_pressed():
-        music.play(song_hello)
+    if button_a.is_pressed(): #버튼 A가 눌렸다면
+        music.play(carol) #캐롤 재생
+    elif button_b.is_pressed(): #B가 눌렸다면
+        music.play(song_hello) #동요 '안녕' 재생
